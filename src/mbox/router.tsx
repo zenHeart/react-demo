10f8571163d3cc31c22b975ffd96348c3be4655f
zenheart <zenheart_register@163.com>
2020-04-26 23:21:14 +0800

    docs: 添加 mbox todo 示例

--- src/mbox/router.tsx
+++ src/mbox/router.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import Nav from '../components/Nav'
 import TimerView from './TimerView'
+import TodoListView from './TodoList';
 
 function Routers() {
   let match = useRouteMatch()
   let config = {
     children: {
-      TimerView
+      TimerView,
+      TodoListView
     },
     parent: match.path
   }
