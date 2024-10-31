3678aa18d434c93b38d28935fa9f57915b1c8760
zenheart <zenheart_register@163.com>
2024-10-31 18:02:06 +0800

    fix: 移除 header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
-       <h1>test</h1>
        <Router>
          {/* @ts-ignore */}
         <Nav children={COMPONENTS} tagsColor={TAGS_COLOR}></Nav>
