ed51eb83cc7adbdac05b03a9939032f21b0bb08e
zenheart <zenheart_register@163.com>
2024-12-09 08:55:55 +0800

    deploy

--- assets/HelloWorld-Q-KjVISi.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CMv67fCt.js";function l(){return o.jsx("h1",{children:"hello world"})}export{l as default};
---
+++ assets/HelloWorld-t-WCldbi.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Rq2bY91f.js";function l(){return o.jsx("h1",{children:"hello world"})}export{l as default};
